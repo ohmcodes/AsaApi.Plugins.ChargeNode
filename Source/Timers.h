e711f2762ee70a7156b845c9e02caeec673c47cf
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2025-03-17 20:57:33 -0700

    Initial commit

---
+++ Source/Timers.h
@@ -0,0 +1,34 @@
+
+void TimerCallback()
+{
+	// sample broadcast every 20secs
+	if (PluginTemplate::counter == 20)
+	{
+		Log::GetLog()->info("Server is up!");
+
+		const FString msg = FString("Welcome to the server");
+		AsaApi::GetApiUtils().GetCheatManager()->Broadcast(&msg);
+	}
+
+	// sample notif every 50secs
+	if (PluginTemplate::counter == 50)
+	{
+		AsaApi::GetApiUtils().SendNotificationToAll(FColorList::Blue, 1.3f, 15.0f, nullptr, "Hooray Welcome to the server! enjoy!");
+	}
+
+
+	PluginTemplate::counter++;
+}
+
+
+void SetTimers(bool addTmr = true)
+{
+	if (addTmr)
+	{
+		AsaApi::GetCommands().AddOnTimerCallback("PluginTemplateTimerTick", &TimerCallback);
+	}
+	else
+	{
+		AsaApi::GetCommands().RemoveOnTimerCallback("PluginTemplateTimerTick");
+	}
+}
